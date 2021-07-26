4f39b5ed44835da0db7cb910fff74f16add8ccc5
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-07-26 13:54:22 +0530

    Updated Apploading

--- App.js
+++ App.js
@@ -2,22 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-import {AppLoading} from 'expo';
-import { require } from 'yargs';
+import AppLoading from 'expo-app-loading';
 
 
 const fetchFont = () =>{
   return Font.loadAsync({
-    'open-sans' : require('./assets/fonts/OpenSans-Reguler.ttf'),
-    'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
 }
 export default function App() {
   const [fontLoaded , setFontLoaded] = useState(false);
   if(!fontLoaded){
-    return (<AppLoading 
+    return (
+    <AppLoading 
       startAsync={fetchFont} 
       onFinish={() => setFontLoaded(true)}
+      onError={(err) => console.log(err)}
       />);
   }
   return (
