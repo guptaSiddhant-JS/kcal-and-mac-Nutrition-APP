ea978816398792bc98fd4cc462bc187f619b9438
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-07-26 12:12:25 +0530

    Added the Diffent Screen that are reqired

---
+++ screens/CategoryMeals.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View , Text , StyleSheet } from "react-native";
+
+const CategoryMeals = props =>{
+    return(<View style={styles.screen}>
+        <Text>The Category Meals</Text>
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    screen : {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+});
+
+export default CategoryMeals;
