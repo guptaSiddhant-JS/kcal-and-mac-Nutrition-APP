7d87a031abe03b327f01f5f8e0c9632cb482e660
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-06 14:21:40 +0530

    Added Dummy Data

---
+++ data/dummy-data.js
@@ -0,0 +1,9 @@
+import Catagory from "../modals/catogary";
+
+export const CATEGORIES =[
+new Catagory('c1' , 'Veg' , 'https://images.creativemarket.com/0.1.0/ps/8750862/1820/1213/m1/fpnw/wm1/nvo0adbzhqgh8vcboifozv1rov9jkdryv6ukp8x3lmtjqxhchrefsolyfnt45n84-.jpg?1595247562&s=f8a11144a4468d7b499264dc97493cea'),
+new Catagory('c2' , 'Non-Veg' , 'https://wallpaperaccess.com/full/1462795.jpg'),
+new Catagory('c3' , 'Vegan' , 'https://images.unsplash.com/photo-1615366105533-5b8f3255ea5d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVnYW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'),
+new Catagory('c4' , 'Keto' , 'https://www.anderzorg.nl/-/m/publieke-sites/anderzorg/images/blogs/anderzorg_eiwit_blog_header.png?w=1024&crop=1&hash=4FD6EDF801E440A98509C4AF69D9F7B7'),
+new Catagory('c5' , 'Dairy-Free' , 'https://mpng.subpng.com/20180203/bae/kisspng-organic-food-juice-vegetable-fruit-basket-vegetable-png-photos-5a755db1e06cc8.3729950115176411379193.jpg'),
+];
